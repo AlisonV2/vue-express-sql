6190a1e02171b8990d1dfc99c6d54678663c5a1c
AlisonV2 <alison.vandromme007@gmail.com>
2021-08-08 12:25:04 +0200

    added db infos to dedicated file

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
 import { Sequelize } from "sequelize";
- 
+import config from './config.js';
+
+const { host, port, user, password, database } = config;
 // create connection
-const db = new Sequelize('express-sql', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
- 
+const db = new Sequelize(
+    database, 
+    user, 
+    password, 
+    {   
+        host: host,
+        dialect: 'mysql' 
+    });
+
 // export connection
 export default db;
